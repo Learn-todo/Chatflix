38dac962165b34b604d6650fa2297f5e0c890535
Goodnessmbakara <amicablembakara50@gmail.com>
2022-10-20 19:58:05 +0100

    working on email auth- let me finish please

--- Backend/chatflix/movie/views.py
+++ Backend/chatflix/movie/views.py
@@ -5,6 +5,7 @@ from django.contrib.sites.shortcuts import get_current_site
 from django.template.loader import render_to_string
 from django.utils.http import urlsafe_base64_encode, urlsafe_base64_decode
 from django.utils.encoding import force_bytes
+from chatflix import settings
 
 # Create your views here.
 def register(request):
@@ -23,7 +24,7 @@ def register(request):
             user.save()
             getting_the_site = get_current_site(request)
             subject = 'Email Activation'
-            message = render_to_string('test.html',
+            message = render_to_string('sign_up.html',
             {
                 'user': user,
                 'domain': getting_the_site.domain,
@@ -38,5 +39,13 @@ def register(request):
 
     # else:
     #    register_form=RegistrationForm()
-    return render(request, 'test.html',{'form': register_form})
+    # Welcome Email
+    subject = "Welcome to GFG- Django Login!!"
+    message = "Hello " + user.first_name + "!! \n" + "Welcome to Chatflix!! \nThank you for visiting our website\n. We have also sent you a confirmation email, please confirm your email address. \n\nThanking You\nGoodness Mbakara"        
+    from_email = settings.EMAIL_HOST_USER
+    to_list = [user.email]
+    send_mail(subject, message, from_email, to_list, fail_silently=True)
+
+    return render(request, 'sign_up.html',{'form': register_form})
+
 
