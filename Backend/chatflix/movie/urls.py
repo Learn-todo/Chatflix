b2a0135c3795c7eb0b0ebb8caf9d612a53daa090
victor <nwosuvictor54@gmail.com>
2023-01-19 00:25:22 +0400

    setup

--- Backend/chatflix/movie/urls.py
+++
@@ -1,7 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path('', views.register, name='register'),
-    path('activate/<slug:uidb64>/<slug:token>/', views.activate, name='activate'),
-]
