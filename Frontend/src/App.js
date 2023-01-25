7297497b73a76abff0bd683c6c763240eed02891
Akuya Ekorot <akuyaekorot@gmail.com>
2023-01-25 19:31:24 +0300

    ui: build third party and email signup buttons

--- Frontend/src/App.js
+++ Frontend/src/App.js
@@ -1,13 +1,19 @@
-import Button from "./components/Button";
 import "./index.css";
+import Button from "./components/Button";
+import SignUpButton from "./components/SignUpButton";
 
 function App() {
   return (
-    <div className={`w-full flex flex-col items-center p-8 gap-4 h-screen`}>
+    <div
+      className={`w-full flex flex-col items-center p-8 gap-4 h-screen bg-white-700`}
+    >
       <Button text={`Primary Button`} type={`primary`} size={`big`} />
       <Button text={`Secondary Button`} type={`secondary`} size={`big`} />
       <Button text={`Sign up`} type={`primary`} size={`small`} />
       <Button text={`Log in`} type={`secondary`} size={`small`} />
+      <SignUpButton icon={`google`} text={`Sign up with Google`} />
+      <SignUpButton icon={`apple`} text={`Sign up with Apple`} />
+      <SignUpButton icon={`email`} text={`Sign up with Email`} />
     </div>
   );
 }
