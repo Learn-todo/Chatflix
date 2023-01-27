2574fcb68d55cc62546d91b7cb7aeb61d4d08527
Gloria <ubahgloria16@gmail.com>
2023-01-28 00:45:21 +0100

    Chore: Added the forgot password page

---
+++ Frontend/src/pages/forgot password/Main.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import Form from "../../auth/forgot password/Form";
+import Slideshow from "../../components/slidesshow/Slideshow";
+
+const ForgotPassword = () => {
+  return (
+    <div>
+      <Slideshow />
+      <Form />
+    </div>
+  );
+};
+
+export default ForgotPassword;
