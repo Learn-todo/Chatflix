09b09294e7cb379671c31e3132250ee48267fde2
Gloria <ubahgloria16@gmail.com>
2023-02-06 16:15:27 +0100

    Chore: code refactor

--- Frontend/src/pages/landing page/Main.jsx
+++ Frontend/src/pages/landing page/Main.jsx
@@ -1,6 +1,4 @@
 import React from "react";
-import style from "../../static/landing page/style.module.css";
-import cardData from "../../static/landing page/cardData";
 import Cta from "../../static/landing page/Cta";
 import Cards from "../../static/landing page/Cards";
 import Faqs from "../../static/landing page/Faqs";
@@ -10,30 +8,13 @@ import Slideshow from "../../static/landing page/Slideshow";
 import Header from "../../static/landing page/Header";
 
 const Main = () => {
-  const data = cardData.map((cards) => {
-    return <Cards key={cards.id} cards={cards} />;
-  });
 
   return (
     <div className="bg-backgroundTwo">
       <Slideshow />
       <Header />
       <Features />
-      <section>
-        <div className="mb-5 text-center">
-          <h3 className="text-arrow">
-            How are we <span className="text-btn-color">different</span>?
-          </h3>
-        </div>
-
-        <div className="mb-5">
-          <div
-            className={`${style._unique} container text-center text-text-color2 mb-4`}
-          >
-            <div className="row mb-5">{data}</div>
-          </div>
-        </div>
-      </section>
+      <Cards />
       <Faqs />
       <Cta />
       <Footer />
--- Frontend/src/static/landing page/Cards.jsx
+++ Frontend/src/static/landing page/Cards.jsx
@@ -1,21 +1,64 @@
 import React from "react";
 import style from "./style.module.css";
+import cardData from "./cardData";
 
-const Cards = (props) => {
+const Cards = () => {
+  const titleColor = (i) => {
+    if (i === 0) {
+      return "text-chat";
+    } else if (i === 1) {
+      return "text-reaction";
+    } else if (i === 2) {
+      return "text-ratings";
+    } else if (i === 3) {
+      return "text-share";
+    } else if (i === 4) {
+      return "text-captions";
+    } else if (i === 5) {
+      return "text-categories";
+    }
+  };
+
   return (
-    <div className={`${style._card_columns} col-4 mb-4`}>
-      <div className={`${style._card} p-4 text-start rounded-1 bg-cards`}>
-        <img
-          src={`./img/${props.cards.img}`}
-          className={`${style._card_img_top}`}
-          alt=""
-        />
-        <div className={`${style._card_body} p-0 py-1`}>
-          <h5 className="card-title py-2 text-chat fs-5">{props.cards.title}</h5>
-          <p className="card-text">{props.cards.description}</p>
+    <section>
+      <div className="mb-5 text-center">
+        <h3 className="text-arrow">
+          How are we <span className="text-btn-color">different</span>?
+        </h3>
+      </div>
+
+      <div className="mb-5">
+        <div
+          className={`${style._unique} container text-center text-text-color2 mb-4`}
+        >
+          <div className="row mb-5">
+            {cardData.map((card, i) => {
+              return (
+                <>
+                  <div className={`${style._card_columns} col-4 mb-4`}>
+                    <div
+                      className={`${style._card} p-4 text-start rounded-1 bg-cards`}
+                    >
+                      <img
+                        src={`./img/${card.img}`}
+                        className={`${style._card_img_top}`}
+                        alt=""
+                      />
+                      <div className={`${style._card_body} p-0 py-1`}>
+                        <h5 className={`${titleColor(i)} card-title py-2 fs-5`}>
+                          {card.title}
+                        </h5>
+                        <p className="card-text">{card.description}</p>
+                      </div>
+                    </div>
+                  </div>
+                </>
+              );
+            })}
+          </div>
         </div>
       </div>
-    </div>
+    </section>
   );
 };
 
