79c48cddf0522ceda49115ad1c2f5bbfdd1e6a4e
Gloria <ubahgloria16@gmail.com>
2023-02-08 21:34:24 +0100

    Chore: setup cloudinary for media

---
+++ Frontend/src/components/logo/ChatflixLogo.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import { Image } from "cloudinary-react";
+
+const Logo = () => {
+  return (
+    <div>
+      <Image
+        cloudName="dfsclcxhm"
+        publicId="Chatflix/State_Full_Logo_jv2vdv.png"
+      />
+    </div>
+  );
+};
+
+export default Logo;
