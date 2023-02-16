a46e83fecb8792f448510afe83f52165d7a61946
Gloria <ubahgloria16@gmail.com>
2023-02-16 22:19:25 +0100

    Chore: slight modifications to styles

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -4,15 +4,16 @@ import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Main from "./pages/landing page/Main";
 import ResetPassword from "./pages/reset password/Main";
 import ForgotPassword from "./pages/forgot password/Main";
+import Dashboard from "./components/dashboard/Dashboard";
 
 const App = () => {
   return(
     <BrowserRouter basename={window.location.pathname || ''}>
       <Routes>
         <Route exact path="/" element = {<Main /> }></Route>
+        <Route path="/dashboard" element = {<Dashboard />} ></Route>
         <Route path="/reset password" element = {<ResetPassword />} ></Route>
         <Route path="/forgot password" element = {<ForgotPassword />} ></Route>
-
       </Routes>
     </BrowserRouter>
   );
