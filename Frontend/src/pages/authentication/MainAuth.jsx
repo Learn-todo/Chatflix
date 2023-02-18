201cb88b4d0803b2866949fcbb985b33be57faef
Gloria <ubahgloria16@gmail.com>
2023-02-18 17:18:37 +0100

    Bug fix

---
+++ Frontend/src/pages/authentication/MainAuth.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import Signup from "../../auth/signup/Signup";
+import Slideshow from "../../components/slidesshow/Slideshow";
+
+const ForgotPassword = () => {
+  return (
+    <div>
+      <Slideshow />
+      <Signup />
+    </div>
+  );
+};
+
+export default ForgotPassword;
