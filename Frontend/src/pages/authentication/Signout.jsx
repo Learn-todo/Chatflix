8195cb462a6972ef64d36658af31c7bd064037c7
Gloria <ubahgloria16@gmail.com>
2023-02-23 20:12:07 +0100

    Feat: Signout page design

---
+++ Frontend/src/pages/authentication/Signout.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import Signout from "../../auth/signout/Signout";
+
+const SignoutPage = () => {
+  return (
+    <div>
+      <Signout />
+    </div>
+  );
+};
+
+export default SignoutPage;
