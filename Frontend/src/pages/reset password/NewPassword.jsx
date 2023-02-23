2af4bf9fc67e28a1c0c116f49223c5f6bcc2205f
Gloria <ubahgloria16@gmail.com>
2023-02-23 18:53:55 +0100

    Feat: Signing page designed with routing.

---
+++ Frontend/src/pages/reset password/NewPassword.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import NewPassword from "../../auth/reset password/SetNewPassword";
+
+const ResetPassword = () => {
+  return (
+    <div>
+      <NewPassword />
+    </div>
+  );
+};
+
+export default ResetPassword;
---
+++ Frontend/src/pages/reset password/Success.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import ResetSuccess from "../../auth/reset password/ResetSuccess";
+
+const ResetPassword = () => {
+  return (
+    <div>
+      <ResetSuccess />
+    </div>
+  );
+};
+
+export default ResetPassword;
