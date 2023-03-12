28d06b1f9a599aa0e2520c71234a125db7e32774
Goodnessmbakara <amicablembakara50@gmail.com>
2023-03-12 01:16:26 +0100

    changed to use sqlite3

--- app/app/settings.py
+++ app/app/settings.py
@@ -90,6 +90,12 @@ WSGI_APPLICATION = 'app.wsgi.application'
 #         'PASSWORD': os.environ.get('DB_PASSWORD'),
 #     }
 # }
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+    }
+}
 
 # Password validation
 # https://docs.djangoproject.com/en/3.2/ref/settings/#auth-password-validators
