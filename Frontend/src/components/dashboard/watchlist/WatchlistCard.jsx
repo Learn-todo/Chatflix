783a5f843bb35c5eda194602481566564230a065
Ademiju Taiwo <hardaymyju@gmail.com>
2023-03-16 18:31:59 +0100

    fixed the white top corners of the images

--- Frontend/src/components/dashboard/watchlist/WatchlistCard.jsx
+++ Frontend/src/components/dashboard/watchlist/WatchlistCard.jsx
@@ -1,12 +1,12 @@
 import React from 'react'
 import {Dot} from 'react-bootstrap-icons';
-import  waitlistStyles from "./waitlist.module.css"
+import  waitlistStyles from "./watchlist.module.css"
 const WatchlistCard = (props) => {
   return (
     <>
       <div className="col gap-y-8">
         <div className={`${waitlistStyles.card} card h-100`}>
-          <img src={props.image} className="card-img-top" alt="..." />
+          <img src={props.image}  alt="..." />
           <div className={`${waitlistStyles.card_body} card-body p-0`}>
             <h5 className={`${waitlistStyles.card_title} card-title`}>{props.title}</h5>
             <p className={`${waitlistStyles.card_text} card-text`}>
