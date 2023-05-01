afc56cfcdad5035d612f3dd396a652f7985b6d71
Gloria <ubahgloria16@gmail.com>
2023-05-01 14:46:13 +0100

    Feat: Added active tab

--- Frontend/src/components/dashboard/Dashboard.jsx
+++ Frontend/src/components/dashboard/Dashboard.jsx
@@ -1,9 +1,8 @@
 import React, { useContext } from "react";
-// import { Outlet } from "react-router-dom";
 import { ToggleContext } from "../../App";
 import style from "./style.module.css";
 import { Image } from "cloudinary-react";
-import { Link } from "react-router-dom"
+import { Link, useLocation } from "react-router-dom"
 import Gloria from "./img/Gloria.jpg";
 import { CgChevronLeftR } from "react-icons/cg";
 import { HiOutlineSparkles } from "react-icons/hi";
@@ -24,6 +23,8 @@ import { BsBoxArrowLeft } from "react-icons/bs";
 
 const Dashboard = ({handleClick}) => {
   const toggle = useContext(ToggleContext);
+  const location = useLocation();
+  const splitLocation = location.pathname;
   
   return (
     <section className={`bg-backgroundTwo text-white p-0 m-0 `}>
@@ -91,109 +92,111 @@ const Dashboard = ({handleClick}) => {
             <small className={`${style._menu_heading}`}>Menu</small>
             <div
               className={
-                toggle
-                  ? `${style._active} ${style._menu_list_show} d-flex justify-center align-items-center`
-                  : `${style._active}`
+                `${toggle
+                  ? `${style._menu_list_show} d-flex justify-center align-items-center`
+                  : ``} ${splitLocation === "/dashboard" ? `${style._active} ${style._menu_list_show_active}` : `` }`
               }
             >
-              <Link to="/dashboard" className={`d-block m-0`}><MdHomeFilled className={`${style._menu_icons}`} /></Link>
-              <Link to="/dashboard">Home</Link>
+                    <Link to="/dashboard" className={`d-block m-0`}>
+                      <MdHomeFilled className={`${style._menu_icons} `} />
+                    </Link>
+                    <Link to="/dashboard">Home</Link>
             </div>
             <div
               className={
-                toggle
-                  ? `${style._active} ${style._menu_list_show} d-flex justify-center align-items-center`
-                  : `${style._active}`
+                `${toggle
+                  ? ` ${style._menu_list_show} d-flex justify-center align-items-center`
+                  : ``} ${splitLocation === "categories" ? `${style._active} ${style._menu_list_show_active}` : `` }`
               }
             >
-              <Link to="" className={`d-block m-0`}>
+              <Link to="/categories" className={`d-block m-0`}>
                 <BsUiChecksGrid className={`${style._menu_icons}`} />
               </Link>
-              <Link to="">Categories</Link>
+              <Link to="/categories">Categories</Link>
             </div>
             <div
               className={
-                toggle
-                  ? `${style._active} ${style._menu_list_show} d-flex justify-center align-items-center`
-                  : `${style._active}`
+                `${toggle
+                  ? `${style._menu_list_show} d-flex justify-center align-items-center`
+                  : ``} ${splitLocation === "upcoming-movies" ? `${style._active} ${style._menu_list_show_active}` : `` }`
               }
             >
-                <Link to="" className={`d-block m-0`}>
+                <Link to="/upcoming-movies" className={`d-block m-0`}>
                   <BiMoviePlay className={`${style._menu_icons}`} />
                 </Link>
-              <Link to="" className={``}>Upcoming Movies</Link>
+              <Link to="/upcoming-movies" className={``}>Upcoming Movies</Link>
             </div>
             <div
               className={`${
                 toggle
-                  ? `${style._active} ${style._menu_list_show} d-flex justify-center align-items-center`
-                  : `${style._active}`
-              } mb-3`}
+                  ? `${style._menu_list_show} d-flex justify-center align-items-center`
+                  : ``
+              } mb-3  ${splitLocation === "watch-history" ? `${style._active} ${style._menu_list_show_active}` : `` }`}
             >
-                <Link to="" className={`d-block m-0`}>
+                <Link to="/watch-history" className={`d-block m-0`}>
                   <BsClock className={`${style._menu_icons}`} />
                 </Link>
-              <Link to="">Watch History</Link>
+              <Link to="/watch-history">Watch History</Link>
             </div>
           </div>
           <div className={`${style._menu_list}`}>
             <small className={`${style._menu_heading}`}>Social</small>
             <div
               className={
-                toggle
-                  ? `${style._active} ${style._menu_list_show} d-flex justify-center align-items-center`
-                  : `${style._active}`
+                `${toggle
+                  ? `${style._menu_list_show} d-flex justify-center align-items-center`
+                  : ``} ${splitLocation === "chat" ? `${style._active} ${style._menu_list_show_active}` : `` }`
               }
             >
-                <Link to="" className={`d-block m-0`}>
+                <Link to="/chat" className={`d-block m-0`}>
                   <IoChatbubblesOutline className={`${style._menu_icons}`} />
                 </Link>
-              <Link to="">Chat</Link>
+              <Link to="/chat">Chat</Link>
             </div>
             <div
               className={`${
                 toggle
-                  ? `${style._active} ${style._menu_list_show} d-flex justify-center align-items-center`
-                  : `${style._active}`
-              } mb-3`}
+                  ? `${style._menu_list_show} d-flex justify-center align-items-center`
+                  : ``
+              } mb-3 ${splitLocation === "community" ? `${style._active} ${style._menu_list_show_active}` : `` }`}
             >
-                <Link to="" className={`d-block m-0`}>
+                <Link to="/community" className={`d-block m-0`}>
                   <HiOutlineUsers className={`${style._menu_icons}`} />
                 </Link>
-              <Link to="">Community</Link>
+              <Link to="/community">Community</Link>
             </div>
           </div>
           <div className={`${style._menu_list}`}>
             <small className={`${style._menu_heading}`}>Library</small>
             <div
               className={
-                toggle
-                  ? `${style._active} ${style._menu_list_show} d-flex justify-center align-items-center`
-                  : `${style._active}`
+                `${toggle
+                  ? `${style._menu_list_show} d-flex justify-center align-items-center`
+                  : ``} ${splitLocation === "activity" ? `${style._active} ${style._menu_list_show_active}` : `` }`
               }
             >
-                <Link to="" className={`d-block m-0`}>
+                <Link to="/activity" className={`d-block m-0`}>
                   <TbActivity className={`${style._menu_icons}`} />
               </Link>
-              <Link to="">Activity</Link>
+              <Link to="/activity">Activity</Link>
             </div>
             <div
               className={
-                toggle
-                  ? `${style._active} ${style._menu_list_show} d-flex justify-center align-items-center`
-                  : `${style._active}`
+                `${toggle
+                  ? `${style._menu_list_show} d-flex justify-center align-items-center`
+                  : ``} ${splitLocation === "top-rated" ? `${style._active} ${style._menu_list_show_active}` : `` }`
               }
             >
-              <Link to="" className={`d-block m-0`}>
+              <Link to="/top-rated" className={`d-block m-0`}>
                 <HiOutlineSparkles className={`${style._menu_icons}`} />
               </Link>
-              <Link to="">Top Rated</Link>
+              <Link to="/top-rated">Top Rated</Link>
             </div>
             <div 
               className={
-                toggle
-                  ? `${style._active} ${style._menu_list_show} d-flex justify-center align-items-center`
-                  : `${style._active}`
+                `${toggle
+                  ? ` ${style._menu_list_show} d-flex justify-center align-items-center`
+                  : ``} ${splitLocation === "/dashboard/watchlist" ? `${style._active} ${style._menu_list_show_active}` : `` }`
               }
             >
                 <Link to="watchlist" className={`d-block m-0`}>
@@ -204,49 +207,49 @@ const Dashboard = ({handleClick}) => {
             <div
               className={`${
                 toggle
-                  ? `${style._active} ${style._menu_list_show} d-flex justify-center align-items-center`
-                  : `${style._active}`
-              } mb-3`}
+                  ? `${style._menu_list_show} d-flex justify-center align-items-center`
+                  : ``
+              } mb-3 ${splitLocation === "downloads" ? `${style._active} ${style._menu_list_show_active}` : `` }`}
             >
-                <Link to="" className={`d-block m-0`}>
+                <Link to="/downloads" className={`d-block m-0`}>
                   <BsBoxArrowInDown className={`${style._menu_icons}`} />
                 </Link>
-              <Link to="">Downloads</Link>
+              <Link to="/downloads">Downloads</Link>
             </div>
           </div>
           <div className={`${style._menu_list}`}>
             <small className={`${style._menu_heading}`}>General</small>
             <div
               className={
-                toggle
-                  ? `${style._active} ${style._menu_list_show} d-flex justify-center align-items-center`
-                  : `${style._active}`
+                `${toggle
+                  ? `${style._menu_list_show} d-flex justify-center align-items-center`
+                  : ``} ${splitLocation === "profile" ? `${style._active} ${style._menu_list_show_active}` : `` }`
               }
             >
-                <Link to="" className={`d-block m-0`}>
+                <Link to="/profile" className={`d-block m-0`}>
                   <CiUser className={`${style._menu_icons}`} />
               </Link>
-              <Link to="">Profile</Link>
+              <Link to="/profile">Profile</Link>
             </div>
             <div
               className={`${
                 toggle
-                  ? `${style._active} ${style._menu_list_show} d-flex justify-center align-items-center`
-                  : `${style._active}`
-              } mb-3`}
+                  ? `${style._menu_list_show} d-flex justify-center align-items-center`
+                  : ``
+              } mb-3 ${splitLocation === "settings" ? `${style._active} ${style._menu_list_show_active}` : `` }`}
             >
-              <Link to="" className={`d-block m-0`}>
+              <Link to="/settings" className={`d-block m-0`}>
                 <BsGear className={`${style._menu_icons}`} />
               </Link>
-              <Link to="">Settings</Link>
+              <Link to="/settings">Settings</Link>
             </div>
           </div>
           <div className={`${style._menu_list} ${style._logout}`}>
             <div
               className={
-                toggle
-                  ? `${style._active} ${style._menu_list_show} d-flex justify-center align-items-center`
-                  : `${style._active}`
+                `${toggle
+                  ? `${style._menu_list_show} d-flex justify-center align-items-center`
+                  : ``} ${splitLocation === "signout" ? `${style._active} ${style._menu_list_show_active}` : `` }`
               }
             >
               <Link to="/signout" className={`d-block m-0`}>
--- Frontend/src/components/home/MovieView.jsx
+++ Frontend/src/components/home/MovieView.jsx
@@ -15,99 +15,180 @@ import Movies from "./Movies";
 const MovieView = ({ movieName, genre }) => {
   const toggle = useContext(ToggleContext);
 
-	// const stars = Array(5).fill(0);
+  // const stars = Array(5).fill(0);
 
-	// const [currenRating, setCurrentRating] = useState(0);
-	// const [hoverValue, setHoverValue] = useState(undefined);
+  // const [currenRating, setCurrentRating] = useState(0);
+  // const [hoverValue, setHoverValue] = useState(undefined);
 
-	// const colors = {
-	// 	orange: "#ffba5a",
-	// 	grey: "#a9a9a9",
-	// };
+  // const colors = {
+  // 	orange: "#ffba5a",
+  // 	grey: "#a9a9a9",
+  // };
 
-	// const handleClick = (value) => {
-	// 	setCurrentRating(value);
-	// };
+  // const handleClick = (value) => {
+  // 	setCurrentRating(value);
+  // };
 
-	// const handloeMouseOver = (value) => {
-	// 	setHoverValue(value);
-	// };
+  // const handloeMouseOver = (value) => {
+  // 	setHoverValue(value);
+  // };
 
-	// const handleMouseLeave = () => {
-	// 	setHoverValue(undefined);
-	// };
+  // const handleMouseLeave = () => {
+  // 	setHoverValue(undefined);
+  // };
 
-	return (
-		<section className={`${style._main} bg-dark pb-0 ${toggle ? `${style._main_resize}` : `${style._main}`}`}>
-			<div className={`mx-2 mt-2 bg-backgroundTwo rounded p-3`}>
-				<div className={`row`}>
-				{Movies.map(movie => {
-					return <div key={movie.id} className={`mb-4 col-md-6`}>
-				<div className={`px-lg-3 pt-lg-3`}>
-				<img src={movie.url} alt="" className={`w-100 h-100`} />
-				<div className={`${style._header_card} p-3 mb-3`}>
-					<div className={`d-flex justify-content-between align-items-center mb-3`}>
-						<div className={`${style._icon_div} text-arrow d-flex justify-content-between align-items-center`}>
-							<BsHeart className={`${style._header_icons}`} />
-							<BsShare className={`${style._header_icons}`} />
-							<CiSaveDown2 className={`${style._header_icons}`} />
-							<FiDownload className={`${style._header_icons}`} />
-						</div>
-						<div className={`d-none ms-3 d-md-flex justify-content-end align-items-center w-75`}>
-						<button className={`${style._download_btn} bg-btn-color btn btn-primary rounded text-center text-arrow mb-2 mb-md-0`}>Stream</button>
-						<button className={`${style._download_btn} btn btn-outline-primary ms-md-3`}>Download</button>
-					</div>
-					</div>
-						<div className={`mb-3`}>
-							<div className={`${style._img_container} position-relative d-flex justify-content-start align-items-center mb-3`}>
-								<div className={``}>
-									<img src={Img} alt="" className={`${style._profile_avatar} position-absolute top-0 left-0`} />
-									<img src={movie.url} alt="" className={`${style._profile_avatar} ${style._profile_avatar2} position-absolute top-0`} />
-									<img src={Img} alt="" className={`${style._profile_avatar} ${style._profile_avatar3} position-absolute top-0`} />
-								</div>
-								<div>
-									<p className={`${style._header_text_size} ${style._header_text_size_comment} text-comments mb-0`}>Liked by<span className={`fw-semibold text-comments`}>&nbsp; Desin_guy</span> and &nbsp;<span className={`fw-semibold text-comments`}>152 others</span></p>
-								</div>
-							</div>
-							<p className={`${style._header_text_size} text-comments mb-1`}>View all 76 comments</p>
-							<div className={`d-flex justify-content-between align-items-center`}>
-								<p className={`${style._header_text_size} text-comments mb-1`}> <span className={`fw-semibold text-comments`}>Toluwa &nbsp;</span>Dope movie. Ya'll should...</p>
-								<BsHeart className={`${style._header_icons} text-arrow`} />
-							</div>
-							<p className={`${style._header_text_size} text-comments mb-1`}>3 hours ago</p>
-							
-						</div>
-					
-					<div className={`d-md-none`}>
-						<button className={`${style._download_btn} d-block bg-btn-color btn btn-primary rounded text-center text-arrow mb-3`}>Stream</button>
-						<button className={`${style._download_btn} btn btn-outline-primary`}>Download</button>
-					</div>
-					<div className={`my-4 border border-bottom border-header-border `}></div>
-					<div className={`d-flex  align-items-center mb-4`}>
-						<div>
-							<img src={Img} alt="" className={`${style._profile_avatar}`} />
-						</div>
-						<div className={`position-relative w-100 ms-4`}>
-							<input
-								type="text"
-								placeholder="Write your comments"
-								name=""
-								id=""
-								className={`${style._input} bg-input p-2 border-0 rounded text-cancel w-100`}
-							/>
-							<BsPaperclip className={`${style._input_icon} ${style._header_icons} text-arrow position-absolute`} />
-							<BsMic  className={`${style._input_icon2} ${style._header_icons} text-arrow position-absolute`}/>
-						</div>
-					</div>
-					
-						</div>
-						</div>
-					</div>
-				})}
-				</div>
-			</div>
-		</section>
-	);
+  return (
+    <section
+      className={`${style._main} bg-dark pb-0 ${
+        toggle ? `${style._main_resize}` : `${style._main}`
+      }`}
+    >
+      <div className={`mx-2 mt-2 bg-backgroundTwo rounded p-3`}>
+        <div className={`row`}>
+          {Movies.map((movie) => {
+            return (
+              <div key={movie.id} className={`mb-4 col-md-6`}>
+                <div className={`px-lg-3 pt-lg-3`}>
+                  <img src={movie.url} alt="" className={`w-100 h-100`} />
+                  <div className={`${style._header_card} p-3 mb-3`}>
+                    <div
+                      className={`d-flex justify-content-between align-items-center mb-3`}
+                    >
+                      <div
+                        className={`${style._icon_div} text-arrow d-flex justify-content-between align-items-center`}
+                      >
+                        <BsHeart className={`${style._header_icons}`} />
+                        <BsShare className={`${style._header_icons}`} />
+                        <CiSaveDown2 className={`${style._header_icons}`} />
+                        <FiDownload className={`${style._header_icons}`} />
+                      </div>
+                      <div
+                        className={`d-none ms-3 d-md-flex justify-content-end align-items-center w-75`}
+                      >
+                        <button
+                          className={`${style._download_btn} bg-btn-color btn btn-primary rounded text-center text-arrow mb-2 mb-md-0`}
+                        >
+                          Stream
+                        </button>
+                        <button
+                          className={`${style._download_btn} btn btn-outline-primary ms-md-3`}
+                        >
+                          Download
+                        </button>
+                      </div>
+                    </div>
+                    <div className={`mb-3`}>
+                      <div
+                        className={`${style._img_container} position-relative d-flex justify-content-start align-items-center mb-3`}
+                      >
+                        <div className={``}>
+                          <img
+                            src={Img}
+                            alt=""
+                            className={`${style._profile_avatar} position-absolute top-0 left-0`}
+                          />
+                          <img
+                            src={movie.url}
+                            alt=""
+                            className={`${style._profile_avatar} ${style._profile_avatar2} position-absolute top-0`}
+                          />
+                          <img
+                            src={Img}
+                            alt=""
+                            className={`${style._profile_avatar} ${style._profile_avatar3} position-absolute top-0`}
+                          />
+                        </div>
+                        <div>
+                          <p
+                            className={`${style._header_text_size} ${style._header_text_size_comment} text-comments mb-0`}
+                          >
+                            Liked by
+                            <span className={`fw-semibold text-comments`}>
+                              &nbsp; Desin_guy
+                            </span>{" "}
+                            and &nbsp;
+                            <span className={`fw-semibold text-comments`}>
+                              152 others
+                            </span>
+                          </p>
+                        </div>
+                      </div>
+                      <p
+                        className={`${style._header_text_size} text-comments mb-1`}
+                      >
+                        View all 76 comments
+                      </p>
+                      <div
+                        className={`d-flex justify-content-between align-items-center`}
+                      >
+                        <p
+                          className={`${style._header_text_size} text-comments mb-1`}
+                        >
+                          {" "}
+                          <span className={`fw-semibold text-comments`}>
+                            Toluwa &nbsp;
+                          </span>
+                          Dope movie. Ya'll should...
+                        </p>
+                        <BsHeart
+                          className={`${style._header_icons} text-arrow`}
+                        />
+                      </div>
+                      <p
+                        className={`${style._header_text_size} text-comments mb-1`}
+                      >
+                        3 hours ago
+                      </p>
+                    </div>
+
+                    <div className={`d-md-none`}>
+                      <button
+                        className={`${style._download_btn} d-block bg-btn-color btn btn-primary rounded text-center text-arrow mb-3`}
+                      >
+                        Stream
+                      </button>
+                      <button
+                        className={`${style._download_btn} btn btn-outline-primary`}
+                      >
+                        Download
+                      </button>
+                    </div>
+                    <div
+                      className={`my-4 border border-bottom border-header-border `}
+                    ></div>
+                    <div className={`d-flex  align-items-center mb-4`}>
+                      <div>
+                        <img
+                          src={Img}
+                          alt=""
+                          className={`${style._profile_avatar}`}
+                        />
+                      </div>
+                      <div className={`position-relative w-100 ms-4`}>
+                        <input
+                          type="text"
+                          placeholder="Write your comments"
+                          name=""
+                          id=""
+                          className={`${style._input} bg-input p-2 border-0 rounded text-cancel w-100`}
+                        />
+                        <BsPaperclip
+                          className={`${style._input_icon} ${style._header_icons} text-arrow position-absolute`}
+                        />
+                        <BsMic
+                          className={`${style._input_icon2} ${style._header_icons} text-arrow position-absolute`}
+                        />
+                      </div>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            );
+          })}
+        </div>
+      </div>
+    </section>
+  );
 };
 
 export default MovieView;
