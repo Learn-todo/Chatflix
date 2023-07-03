5e40953d968c485402ab25294cf87e5fb790e9b5
Gloria <ubahgloria16@gmail.com>
2023-07-03 13:47:26 +0100

    Feat: Added chat room friends

---
+++ Frontend/src/components/chats/chatroomdata.js
@@ -0,0 +1,50 @@
+import Img1 from "./img/man1.jpg";
+import Img2 from "./img/man2.jpg";
+import Img3 from "./img/man4.jpg";
+import Img4 from "./img/woman1.jpg";
+import Img5 from "./img/woman2.jpg";
+
+const friendsData = [
+     {
+          img: Img1,
+          name: "Toluwa"
+     },
+     {
+          img: Img2,
+          name: "Design_guy"
+     },
+     {
+          img: Img3,
+          name: "The_optimust"
+     },
+     {
+          img: Img4,
+          name: "Increase"
+     },
+     {
+          img: Img5,
+          name: "Gloria"
+     },
+     {
+          img: Img1,
+          name: "Goodness"
+     },
+     {
+          img: Img2,
+          name: "Victor_dev"
+     },
+     {
+          img: Img3,
+          name: "Ademijuwonlo"
+     },
+     {
+          img: Img4,
+          name: "Dev_patrick"
+     },
+     {
+          img: Img5,
+          name: "Jibo"
+     }
+]
+
+export default friendsData;
